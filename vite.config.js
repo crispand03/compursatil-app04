3287315e61b0612f083bc71e6d72916b46f1e790
crispand03 <crispand03@github.com>
2025-12-01 23:06:17 -0500

    Clean up vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-//export default {
-  //server: {
-  //  host: '0.0.0.0'
-  //}
-//}
-//
-
-
 export default defineConfig({
   plugins: [react()],
   base: "/compursatil-app02",
